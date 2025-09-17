5801e6efeadba69c4353b0bd63af469109e18fb5
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-09-17 17:19:28 +0200

    Add files via upload

---
+++ 1º/Programacion/Tema 4/Array unidimensionales++/ejercicio1.java
@@ -0,0 +1,149 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int TOTAL_EMPLEADOS=10;
+        int opcion,totalEmpleados=0;
+        Empleado[] empleado=new Empleado[TOTAL_EMPLEADOS];
+        String dni;
+        double edadMedia=0;
+
+        /**
+         * Para introducir los parametros de los usuarios
+         */
+        String intNombre, intApellidos,intDNI;
+        int intEdad, intSueldo;
+        do {
+            System.out.println("Introduduzca valor: ");
+            opcion=sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
+                case 1:
+                    if (totalEmpleados<TOTAL_EMPLEADOS) {
+                        System.out.print("Introduce nombre: ");
+                        intNombre = sc.nextLine();
+                        System.out.print("Introduzca apellidos: ");
+                        intApellidos = sc.nextLine();
+                        do {
+                            System.out.print("Introduce el DNI: ");
+                            intDNI = sc.nextLine();
+                        } while (intDNI.length() != 9);
+                        System.out.print("Introduce la edad: ");
+                        intEdad = sc.nextInt();
+                        System.out.print("Introduce el sueldo: ");
+                        intSueldo = sc.nextInt();
+                        empleado[totalEmpleados] = new Empleado(intDNI,intNombre,intApellidos,intEdad,intSueldo);
+                        totalEmpleados++;
+                    } else
+                        System.out.println("No se pueden introducir mas empleados");
+                    break;
+                case 2:
+                    if (totalEmpleados==0)
+                        System.out.println("No hay empleados");
+                    else {
+                        for (int i=0;i<totalEmpleados;i++) {
+                            System.out.println("--- Empleado "+(i+1)+" ---");
+                            System.out.println(empleado[i].toString());
+                        }
+                    }
+                    break;
+                case 3:
+                    if (totalEmpleados==0)
+                        System.out.println("No hay empleados");
+                    else {
+                        System.out.println("Introduzca un dni: ");
+                        dni=sc.nextLine();
+                        for (int i=0; i<totalEmpleados;i++) {
+                            if (empleado[i].getDni().equals(dni)) 
+                                System.out.println(empleado[i].toString());
+                        }
+                    }
+                    break;
+                case 4:
+                    int aux=0;
+                    if (totalEmpleados==0)
+                        System.out.println("No hay empleados");
+                    else {
+                        for (int i=0; i<totalEmpleados; i++) {
+                            aux += empleado[i].getEdad();
+                        }
+                        edadMedia = (double)aux/totalEmpleados;
+                    }
+                    break;
+                case 5:
+                    int pos;
+                    do {
+                        System.out.println("Introduzca una posicion:");
+                        pos=sc.nextInt();
+                    } while (pos>(totalEmpleados+1));
+                    System.out.println("El sueldo del empleado de la posicion "+pos+" es: "+empleado[pos].getSueldo());
+                    break;
+                case 6:
+                    if (totalEmpleados==0)
+                        System.out.println("No hay empleados");
+                    else {
+                        String nuevoNombre;
+                        System.out.println("Introduzca un dni: ");
+                        dni=sc.nextLine();
+                        for (int i=0; i<totalEmpleados;i++) {
+                            if (empleado[i].getDni().equals(dni)) {
+                                System.out.print("Introduce el nuevo nombre: ");
+                                nuevoNombre=sc.nextLine();
+                                empleado[i].setNombre(nuevoNombre);
+                            } 
+                        }
+                    }
+                    break;
+                case 7:
+                    if (totalEmpleados==0)
+                        System.out.println("No hay empleados");
+                    else {
+                        int nuevaEdad;
+                        System.out.println("Introduzca un dni: ");
+                        dni=sc.nextLine();
+                        for (int i=0; i<totalEmpleados;i++) {
+                            if (empleado[i].getDni().equals(dni)) {
+                                System.out.print("Introduce la nueva edad: ");
+                                nuevaEdad=sc.nextInt();
+                                empleado[i].setEdad(nuevaEdad);
+                            } 
+                        }
+                    }
+                    break;
+                case 8:
+                    if (totalEmpleados==0)
+                        System.out.println("No hay empleados");
+                    else {
+                        int nuevoSueldo;
+                        System.out.println("Introduzca un dni: ");
+                        dni=sc.nextLine();
+                        for (int i=0; i<totalEmpleados;i++) {
+                            if (empleado[i].getDni().equals(dni)) {
+                                System.out.print("Introduce el nuevo sueldo: ");
+                                nuevoSueldo=sc.nextInt();
+                                empleado[i].setEdad(nuevoSueldo);
+                            } 
+                        }
+                    }
+                    break;
+                case 9:
+                    if (totalEmpleados==0)
+                        System.out.println("No hay empleados");
+                    else {
+                        Empleado empleadoAUX = new Empleado();
+                        for (int i=0; i<totalEmpleados;i++) {
+                            if (empleadoAUX.getSueldo()<=empleado[i].getSueldo()) 
+                                empleadoAUX=empleado[i];
+                        }
+                        System.out.println("Los datos del empleado con mayor sueldo es:");
+                        System.out.println(empleadoAUX.toString());
+                    }
+                    break;
+                default: 
+                    System.out.println("Se sale del programa");
+                    break;
+            }
+        } while (opcion < 10);
+    }    
+}
---
+++ 1º/Programacion/Tema 4/Entregas/RegEx.java
@@ -0,0 +1,84 @@
+/**
+ * RegEx Helper
+ * @author Sergio Trillo
+ */
+import java.util.regex.*;
+
+public class RegEx {
+    /**
+     * Método para validar el formato de un DNI (8 dígitos y una letra)
+     * @param dni
+     * @return true si es valido, false si no lo es
+     */
+    public static boolean validarDNI(String dni) {
+        String regex = "\\d{8}[A-Za-z]";
+        return Pattern.matches(regex, dni);
+    }
+
+    /**
+     * Método para validar el formato de un pasaporte (3 letras y 6 dígitos)
+     * @param pasaporte
+     * @return true si es valido, false si no lo es
+     */
+    public static boolean validarPasaporte(String pasaporte) {
+        String regex = "[A-Za-z]{3}\\d{6}";
+        return Pattern.matches(regex, pasaporte);
+    }
+
+    /**
+     * Método para validar el formato de un código IBAN de cuentas bancarias en España (ES más 22 dígitos)
+     * @param iban
+     * @return true si es valido, false si no lo es
+     */
+    public static boolean validarIBAN(String iban) {
+        String regex = "ES\\d{2}(\\s|\\-)?\\d{4}(\\s|\\-)?\\d{4}(\\s|\\-)?\\d{2}(\\s|\\-)?\\d{10}";
+        return Pattern.matches(regex, iban.replaceAll("[\\s\\-]", ""));
+    }
+
+    /**
+     * Método para validar números de teléfonos en España (6, 7 o 9 seguido de 8 dígitos)
+     * @param telefono
+     * @return true si es valido, false si no lo es
+     */
+    public static boolean validarTlfnEspana(String telefono) {
+        String regex = "(\\+34)?[6|7|9]\\d{8}";
+        return Pattern.matches(regex, telefono.replaceAll("[\\s\\-]", ""));
+    }
+
+    /**
+     * Método para validar fechas en formatos dd/mm/yyyy o dd-mm-yyyy
+     * @param fecha
+     * @return true si es valido, false si no lo es
+     */
+    public static boolean validarFechas(String fecha) {
+        String regex = "\\d{2}[\\/\\-]\\d{2}[\\/\\-]\\d{4}";
+        return Pattern.matches(regex, fecha);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Validar DNI:");
+        System.out.println("12345678Z: "+validarDNI("12345678Z"));
+        System.out.println("1234A: "+validarDNI("1234A")); 
+
+        System.out.println("\nValidar Pasaporte:");
+        System.out.println("ABC123456: "+validarPasaporte("ABC123456"));
+        System.out.println("A1C123: "+validarPasaporte("A1C123"));
+
+        System.out.println("\nValidar IBAN:");
+        System.out.println("ES9121000418450200051332: "+validarIBAN("ES9121000418450200051332"));      
+        System.out.println("ES91 21 0004 1845 02 00051332: "+validarIBAN("ES91 21 0004 1845 02 00051332"));
+        System.out.println("ES91-21-0004-1845-02-00051332: "+validarIBAN("ES91-21-0004-1845-02-00051332"));
+        System.out.println("ES21: "+validarIBAN("ES21"));                     
+
+        System.out.println("\nValidar Teléfono en España:");
+        System.out.println("+34612345678: "+validarTlfnEspana("+34612345678"));
+        System.out.println("612345678: "+validarTlfnEspana("612345678"));   
+        System.out.println("812345678: "+validarTlfnEspana("812345678"));
+
+        System.out.println("\nValidar Fechas:");
+        System.out.println("10/10/2000: "+validarFechas("10/10/2000"));
+        System.out.println("21-12-2001: "+validarFechas("21-12-2001"));
+        System.out.println("21/12-2001: "+validarFechas("21/12-2001"));
+        System.out.println("21/12-01: "+validarFechas("21/12-01"));
+    }
+}
